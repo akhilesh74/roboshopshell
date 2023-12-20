b736579f99d98b4e58ae8ed9cc7342a8497f6750
akhilesh74 <akhilehakhi1993@gmail.com>
2023-12-20 12:43:34 +0530

    added the script

---
+++ mongodb.sh
@@ -0,0 +1,34 @@
+#!bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started exicuted at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [$1 -ne 0]
+    then
+        echo -e "$2...$R failed $N"
+    else 
+        echo -e "$2...$G success $N"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then 
+    echo -e "$R Error: please use root $N"
+    exit 1
+else
+    echo "yau are in root"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copied mongo repo"
